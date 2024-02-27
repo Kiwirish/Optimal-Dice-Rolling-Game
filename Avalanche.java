38191ae4206548369315ca58a712f02c97590d14
Matthew MacDonald <113599350+MatthewMacDonald-1@users.noreply.github.com>
2024-02-28 12:45:34 +1300

    Made our implementation class

---
+++ Avalanche.java
@@ -0,0 +1,12 @@
+public class Avalanche extends Rollin {
+
+    public Avalanche() {
+        
+    }
+
+    @Override
+    public int handleRoll(int roll, int[] dice) {
+        return -1;
+    }
+    
+}
