f56bc205a25b8de373b6d32e0a36f624edacee43
Matthew MacDonald <113599350+MatthewMacDonald-1@users.noreply.github.com>
2024-02-28 13:32:43 +1300

    Added first check and a brute force and a copy utility

--- Avalanche.java
+++ Avalanche.java
@@ -1,4 +1,8 @@
+import java.util.Random;
+
 public class Avalanche extends Rollin {
+
+    Random R = new Random();
 
     public Avalanche() {
 
@@ -7,12 +11,32 @@ public class Avalanche extends Rollin {
     @Override
     public int handleRoll(int roll, int[] dice) {
         // Check for complete
+        if (Rollin.isComplete(dice)) {
+            return -1;
+        }
 
-        // Check brute fore complete
+        // Check brute force complete
+        for (int i = 0; i < dice.length; i++) {
+            int[] copy = CopyArray(dice);
+
+            copy[i] = roll;
+            if (Rollin.isComplete(copy)) {
+                return i;
+            }
+        }
+
 
         // decide which one to change if not instant complete
 
-        return 0;
+        return R.nextInt(6);
+    }
+
+    private int[] CopyArray(int[] a) {
+        int[] b = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i]; 
+        }
+        return b;
     }
     
 }
