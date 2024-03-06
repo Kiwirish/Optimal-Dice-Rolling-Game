3e3b73f58f98a6e0f64f1c1b5429453aa8a7bd14
rulja771 <rulja771@student.otago.ac.nz>
2024-03-06 13:30:45 +1300

    jamies stuff

--- jamiesTest.java
+++ jamiesTest.java
@@ -16,7 +16,7 @@ public class jamiesTest {
         System.out.println("Initial dice: " + java.util.Arrays.toString(d));
         int roll = R.nextInt(6) + 1;
         System.out.println("Roll: " + roll);
-        System.out.println("Outs: " + jamies.handleRoll(roll, d));
+        //System.out.println("Outs: " + jamies.handleRoll(roll, d));
         
 
         while (!Rollin.isComplete(d)) {
@@ -24,9 +24,11 @@ public class jamiesTest {
             System.out.println("Roll: " + roll);
             int toChange = rand.handleRoll(roll, d);
             System.out.println("RandomRoller changes: " + toChange);
+            //int toChange = jamies.handleRoll(roll, d);
+            System.out.println("indice change: " + toChange);
             d[toChange] = roll;
             System.out.println("Dice: " + java.util.Arrays.toString(d));
-            System.out.println("Outs: " + jamies.handleRoll(roll, d));
+            //System.out.println("indice to change: " + jamies.handleRoll(roll, d));
         }
     }
   
