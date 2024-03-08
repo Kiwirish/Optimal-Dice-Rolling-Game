ce1a0466911c586f44da603cfd6028b001293bf4
Matthew MacDonald <113599350+MatthewMacDonald-1@users.noreply.github.com>
2024-03-08 13:27:37 +1300

    More testing

--- Test.java
+++ Test.java
@@ -79,8 +79,10 @@ public class Test {
             RunTestGeneric(numOfTrials, new Avalanche(), "Aval");
             RunTestGeneric(numOfTrials, new PlusOrMinusOne(), "Pomo");
             RunTestGeneric(numOfTrials, new AvalancheProbSel(), "ProbSel");
-            RunTestGeneric(numOfTrials, new JamiesAttempt(), "Jamies"); // Has better worst case time then the other algorithms at ~1.37 ms but has the worst average case performance at ~0.018 ms which is ~4x slower then ProbSel and ~8x slower then Pomo
+            // RunTestGeneric(numOfTrials, new JamiesAttempt(), "Jamies"); // Has better worst case time then the other algorithms at ~1.37 ms but has the worst average case performance at ~0.018 ms which is ~4x slower then ProbSel and ~8x slower then Pomo
+            RunTestGeneric(numOfTrials, new FirstAttemptBlake(), "Blake");
             RunTestGeneric(numOfTrials, new JamiesAttemptMod(), "JamiesMod");
+
         }        
     }
 
